cf281ca76568c96e591fbbc40c02614cb14ff994
Muhammad Enamul Huq Sarkar <oneenam@gmail.com>
2018-03-31 13:55:46 +0200

    testing login

--- db/database.js
+++ db/database.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp");
+mongoose.connect(process.env.MONGODB_URI);
 //mongoose.connect("mongodb://testuser:asdf12345678@ds229909.mlab.com:29909/todoapp");
 
 module.exports = { mongoose };

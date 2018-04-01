851ca5db37ac8b6c3d3c362ba1d281462db88f6d
Muhammad Enamul Huq Sarkar <oneenam@gmail.com>
2018-04-01 17:20:34 +0200

    testing

--- models/model.user.js
+++ models/model.user.js
@@ -46,7 +46,8 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
     var user = this;
     var access = 'auth';
-    var token = jwt.sign({ _id: user._id.toHexString(), access }, process.env.JWT_SECRET).toString();
+    //var token = jwt.sign({ _id: user._id.toHexString(), access }, process.env.JWT_SECRET).toString();
+    var token = jwt.sign({ _id: user._id.toHexString(), access }, "nodejs3000express3000mongodb2018").toString();
 
     user.tokens.push({ access, token });
 

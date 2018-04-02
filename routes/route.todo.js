e3bd26e8c052cc52a25c909f0e74d59be773d61b
Muhammad Enamul Huq Sarkar <oneenam@gmail.com>
2018-04-02 23:23:53 +0200

    todo find and update

--- routes/route.todo.js
+++ routes/route.todo.js
@@ -65,10 +65,10 @@ module.exports = app => {
 
 
     // update/complete todo
-    app.patch('/todos', authenticate, (req, res) => {
+    app.post('/todos/update', authenticate, (req, res) => {
         //var id = req.params.id;
         var body = _.pick(req.body, ['id','text', 'completed']);
-        res.send({ id: req.body.id });
+        res.send({ body });
 
         if (!ObjectID.isValid(id)) {
             return res.status(404).send();
